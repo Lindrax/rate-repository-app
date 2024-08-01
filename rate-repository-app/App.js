f41408a9c15ca8b7f9dca4ffcf5ae4dd11d0f503
Axel <->
2024-08-01 11:50:31 +0300

    fixes

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,18 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-
+import { ApolloProvider } from '@apollo/client';
 import { NativeRouter } from 'react-router-native';
 
 import Main from './src/components/Main';
+import createApolloClient from './src/utils/apolloClient';
+
+const apollOClient = createApolloClient()
 
 const App = () => {
   return (
-
-    <>
-      <NativeRouter>
+    <NativeRouter>
+      <ApolloProvider client={apollOClient}>
         <Main />
-      </NativeRouter>
-      <StatusBar style="auto" />
-    </>
+      </ApolloProvider>
+    </NativeRouter>
   );
 };
 
